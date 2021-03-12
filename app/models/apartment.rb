59b6f8d1450d0d04053a110832f32063f5dd8887
lizburkechi <liz.burke.chi@gmail.com>
2021-03-12 11:55:57 -0600

    some photos added

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,8 +3,10 @@ class Apartment < ApplicationRecord
     has_many :renters, through: :renter_apartment_applications
     has_one :pet_policy
 
-    def self.find_by_restrictions(weight, breed)
-        PetPolicy.where("weight_max > ? AND NOT breed_restriction = ?", weight, breed)
-    end
-    
+    # def self.find_by_restrictions(rent_allowance)
+    #     rent =  Apartment.where(“rent_allowance > ? “, rent_allowance)
+    #         rent.map do |p|
+    #         p.apartment
+    #         end
+    # end
 end
--- app/models/pet_policy.rb
+++ app/models/pet_policy.rb
@@ -2,14 +2,14 @@ class PetPolicy < ApplicationRecord
   belongs_to :apartment
 
 
-  def self.find_by_restrictions(weight, breed)
-  found_policies =  PetPolicy.where("weight_max > ? AND NOT breed_restriction = ?", weight, breed)
+  # def self.find_by_restrictions(weight, breed)
+  # found_policies =  PetPolicy.where("weight_max > ? AND NOT breed_restriction = ?", weight, breed)
 
-    found_policies.map do |p|
-      p.apartment
-    end
+  #   found_policies.map do |p|
+  #     p.apartment
+  #   end
 
-  end
+  # end
 
 
 
